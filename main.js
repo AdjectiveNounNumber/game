f2346d33dca14d903df86a0a577386a60cbe76bf
AdjectiveNounNumber <alexander.krauter@outlook.com>
2026-06-22 08:00:40 -0400

    edit code

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 let n = 0;
 let mx = my = 0;
+let birth = Date.now();
+let cheating = false;
 function resize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
@@ -31,12 +33,23 @@ function draw() {
     ctx.fillText(`You clicked the circle ${n} time${n == 1 ? '' : 's'}`, w/2, h/2 + 80);
     ctx.font = '16px Arial';
     ctx.fillText('Click the circle to 100', w/2, h/2 + 120);
-    if (n > 119) {
+    if (n > 124 && n < 149 && !cheating) {
         ctx.font = '14px Arial';
         ctx.fillText('ok genuinely stop there is nothing more to see here', w/2, h/2 - 100);
+    } else if (n > 149) {
+        ctx.font = '14px Arial';
+        ctx.fillText("well, since you're still here this was originally made as a task in a discord server", w/2, h/2 - 100);
+    } else if (n > 174) {
+        ctx.font = '14px Arial';
+        ctx.fillText("uhm. im gonna go now, bye", w/2, h/2 - 100);
     }
     if (n > 99) {
         ctx.font = '24px Arial';
+        if (Date.now() - birth < 8000 || cheating) {
+            cheating = true;
+            ctx.fillStyle = 'red';
+            ctx.fillText('too fast, cheater >:(', w/2, h/2 - 70);
+        }
         ctx.fillStyle = 'green';
         ctx.fillText('Good job you win!', w/2, h/2 - 70);
     }
